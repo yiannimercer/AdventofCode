e3b645696a9b9425ad722b34500ff61c2c32c74e
Yianni <YJMercer@gmail.com>
2022-12-18 01:26:13 -0600

    Day17 initial committ

---
+++ 2022/Day17_YM.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+from aocd import get_data, submit
+
+# --------------------------------------------------------------------
+# DAY 17 PUZZLE INPUT
+# --------------------------------------------------------------------
+
+data = get_data(day=17)
