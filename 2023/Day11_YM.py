1b6baa7b701cb087b60c9d9fb59ffb746d7a2452
Yianni (John) Mercer <YJMercer@gmail.com>
2023-12-11 20:47:59 -0600

    Day 11 - Incomplete

--- 2023/Day11_YM.py
+++ 2023/Day11_YM.py
@@ -1,19 +1,46 @@
 from aocd import get_data, submit
 from dotenv import load_dotenv
+
 load_dotenv()
 
 
-#------------------------------------------------
-# READ DATA IN 
-#------------------------------------------------
+# ------------------------------------------------
+# READ DATA IN
+# ------------------------------------------------
 
-data = get_data(day=11, year=2023).splitlines()
+image = get_data(day=11, year=2023).splitlines()
 
-#------------------------------------------------
-# PART 1 
-#------------------------------------------------
+# ------------------------------------------------
+# PART 1
+# ------------------------------------------------
 
 # Expanding the Universe
+
+
+def find_empty_space_indexes(image_input):
+    # Finding empty rows
+    empty_rows = [
+        i for i, row in enumerate(image_input) if all(ch == "." for ch in row)
+    ]
+
+    # Finding empty columns
+    empty_columns = []
+    for j in range(len(image_input[0])):
+        if all(image_input[i][j] == "." for i in range(len(image_input))):
+            empty_columns.append(j)
+
+    return empty_rows, empty_columns
+
+
+empty_space_row_index, empty_space_column_index = find_empty_space_indexes(image)
+
+
+def expand_universe(image_input, empty_rows, empty_columns):
+    num_rows, num_cols = len(image_input), len(image_input[0])
+    
+    # Add new rows
+    
+
 
 # Identify Pairs
 
@@ -22,4 +49,3 @@ data = get_data(day=11, year=2023).splitlines()
 # Calculate Sum of each Distance
 
 # Submit Part 188765
-
