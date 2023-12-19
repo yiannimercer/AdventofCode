7d08c9fe669be21ffa783898f497e945b179b7f5
Yianni Mercer <yiannimercer@Yiannis-MacBook-Pro.local>
2023-12-19 08:31:03 -0600

    Day 17

--- 2023/Day17_YM.py
+++ 2023/Day17_YM.py
@@ -1,5 +1,6 @@
 from aocd import get_data, submit
 from dotenv import load_dotenv
+from heapq import *
 
 load_dotenv()
 
@@ -27,4 +28,92 @@ example_data = ['2413432311323',
 # PART 1
 # --------------------------------------------------------------------
 
+
+def min_heat_path(map_data) -> None:
+
+    width = len(map_data[0])
+    height = len(map_data)
+
+    directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    heap = [(0, 0, 0, 0, 0), (0, 0, 0, 1, 0)]
+    seen = set()
+
+    while len(heap) > 0:
+        heat, x, y, direction, streak = heappop(heap)
+
+        if (x, y, direction, streak) in seen:
+            continue
+
+        seen.add((x, y, direction, streak))
+
+        if x == width - 1 and y == height - 1:
+            print(heat)
+            return heat
+
+        dx, dy = directions[direction]
+        x += dx
+        y += dy
+
+        if x < 0 or x >= width or y < 0 or y >= height:
+            continue
+
+        heat += int(map_data[y][x])
+
+
+        if streak < 2:
+            heappush(heap, (heat, x, y, direction, streak + 1))
+
+        heappush(heap, (heat, x, y, (direction + 1) % 4, 0))
+        heappush(heap, (heat, x, y, (direction - 1) % 4, 0))
+        
+        
+min_heat = min_heat_path(map_data)
+
+# --------------------------------------------------------------------
+# PART 2
+# --------------------------------------------------------------------
+
+
+def min_heat_path_part2(map_data) -> None:
     
+    width = len(map_data[0])
+    height = len(map_data)
+
+    directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    heap = [(0, 0, 0, 0, 0), (0, 0, 0, 1, 0)]
+    seen = set()
+
+    while len(heap) > 0:
+        heat, x, y, direction, streak = heappop(heap)
+
+        if (x, y, direction, streak) in seen:
+            continue
+
+        seen.add((x, y, direction, streak))
+
+        if x == width - 1 and y == height - 1:
+            if streak > 3:
+                print(heat)
+                return heat
+
+        dx, dy = directions[direction]
+        x += dx
+        y += dy
+
+        if x < 0 or x >= width or y < 0 or y >= height:
+            continue
+
+        heat += int(map_data[y][x])
+
+
+        if streak < 10:
+            heappush(heap, (heat, x, y, direction, streak))
+
+        if streak > 3:
+            heappush(heap, (heat, x, y, (direction + 1) % 4, 0))
+            heappush(heap, (heat, x, y, (direction - 1) % 4, 0))
+
+
+min_heat_part2 = min_heat_path_part2(map_data)
+
+submit(min_heat, part='a', day=17, year=2023)
